3daa6abf052ce9bb77d90d20ee9826553d6e8a07
vinita gupta <vinita.gupta@sapient.com>
2019-04-15 18:20:17 +0530

    added components communication

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,21 +1,36 @@
 <template>
     <div>
         <div class="col-xs-12 col-sm-6">
-            <p> {{ contentMessage }} </p>
+            <p v-if="!server">Please select a server to display details</p>
+            <p v-else>Server #{{server.id}} is selected,Server status: {{server.status}}</p>
+            <hr>
+            <button @click="resetStatus">Change Status</button>
         </div>
         
     </div>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        contentMessage: 'Server Details are currently not updated'
-      }
+    import { serverBus } from '../../main';
+
+    export default {
+        data: function() {
+            return{
+                server: null
+            }
+        },
+        methods: {
+            resetStatus(){
+                this.server.status= 'Normal'
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected',(server) => {
+                this.server = server;
+            })
+        }
     }
-  }
 </script>
 
 <style>
-</style
+</style>
--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,21 +1,33 @@
 <template>
-    <div>
-        <div class="col-xs-12 col-sm-6">
-            <ul class="list-group">
-                <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                    Server #{{ index }}
-                </li>
-            </ul>
-        </div>
-       
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server
+                v-for="server in servers"
+                :server="server"></app-server>
+        </ul>
     </div>
 </template>
 
 <script>
-  
+    import Server from './Server.vue';
+
+    export default {
+        data: function() {
+            return {
+              servers: [
+                  { id: 1, status: 'Normal'},
+                  { id: 2, status: 'Critical'},
+                  { id: 3, status: 'Unknown'},
+                  { id: 4, status: 'Normal'}
+              ]
+            };
+        },
+        components: {
+            appServer: Server
+        }
+    }
 </script>
 
 <style>
-</style
+
+</style>
